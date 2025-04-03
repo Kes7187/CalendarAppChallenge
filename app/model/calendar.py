428e0024520d809dae058196ba75e111fa1fa7db
kesma <kesmanp7187@gmail.com>
2025-04-03 13:52:28 -0500

    add class Reminder, class Event and class day

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -6,13 +6,61 @@ from app.services.util import generate_unique_id, date_lower_than_today_error, e
     reminder_not_found_error, slot_not_available_error
 
 
-# TODO: Implement Reminder class here
+@dataclass
+class Reminder:
+    EMAIL: ClassVar[str] = "email"
+    SYSTEM: ClassVar[str] = "system"
+
+    date_time: datetime
+    type: str = EMAIL
+
+    def __str__(self) -> str:
+        return f"Reminder on {self.date_time} of type {self.type}"
 
 
-# TODO: Implement Event class here
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder] = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
+
+    def add_reminder(self, date_time: datetime, type: str = Reminder.EMAIL):
+        self.reminders.append(Reminder(date_time, type))
+
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+
+    def __str__(self) -> str:
+        return (f"ID: {self.id}\n"
+                f"Event title: {self.title}\n"
+                f"Description: {self.description}\n"
+                f"Time: {self.start_at} - {self.end_at}")
 
 
-# TODO: Implement Day class here
 
+class Day:
+    def __init__(self, date_: date):
+        self.date_: date_
+        self.slots: dict[time, str] = {}
+        self._init_slots()
 
-# TODO: Implement Calendar class here
+    def _init_slots(self):
+        for hour in range(24):
+            for minute in range(0, 60, 15):
+                slot_time = time(hour, minute)
+                self.slots[slot_time] = None
+
+    def add_event(self, event_id: str, start_at: time, end_at: time):
+        current_slot = start_at
+        while current_slot < end_at:
+            if current_slot not in self.slots or self.slots[current_slot] is not None:
+                slot_not_available_error()
+            current_slot = time(current_slot.hour, current_slot.minute + 15)
+
